b1df51f27b8ddd47f88f9d1728c74f3507484feb
ascoelho <anthonystefancoelho@gmail.com>
2016-05-10 20:00:17 -0400

    Complete Assignment

---
+++ Auto Layout - Storyboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Auto Layout - Storyboard
+//
+//  Created by Anthony Coelho on 2016-05-10.
+//  Copyright © 2016 Vectura. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Auto Layout - Storyboard/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Auto Layout - Storyboard
+//
+//  Created by Anthony Coelho on 2016-05-10.
+//  Copyright © 2016 Vectura. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
